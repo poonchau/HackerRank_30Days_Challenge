f7781aceaeba7e31ce19bca85ce85bd563d352e3
Poonam Chaurasia <62844354+poonchau@users.noreply.github.com>
2021-03-15 15:16:09 +0530

    Add files via upload

---
+++ Calculate_Total_RestaurantBill.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the solve function below.
+    static void solve(double meal_cost, int tip_percent, int tax_percent) {
+         double T_tip;
+         double T_tax;
+         double Total;
+
+          T_tip = (tip_percent * meal_cost) / 100;
+          T_tax = (tax_percent * meal_cost) / 100;
+          Total = (meal_cost + T_tip +T_tax);
+          
+          
+          
+          Console.WriteLine(Math.Round(Total));
+
+    }
+
+    static void Main(string[] args) {
+        double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+        int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+        int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+        solve(meal_cost, tip_percent, tax_percent);
+    }
+}
+
