55560280e36076e5db68ca6dfd0853cb56465e84
Poonam Chaurasia <62844354+poonchau@users.noreply.github.com>
2021-04-10 12:05:05 +0530

    Update calculateTotalMarksObtained.cs

--- calculateTotalMarksObtained.cs
+++ calculateTotalMarksObtained.cs
@@ -1,3 +1,53 @@
+// Using List<char> OR List<string>
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Test
+{
+	public static void Main()
+	{
+		// your code goes here
+		List<char> list1 = new List<char>();
+		List<char> list2 = new List<char>();
+		
+		list1.Add('a');
+		list1.Add('b');
+		list1.Add('c');
+		list1.Add('d');
+		
+		list2.Add('a');
+		list2.Add('p');
+		list2.Add('q');
+		list2.Add('r');
+	
+	
+	int pos=0, neg=0;
+	for (int i = 0; i < list1.Count; i++){
+	    if (list1[i] == list2[i])
+	{
+	    pos++;
+	}
+	else{
+	    neg++;
+	}
+	}
+	
+	int res = (pos * 4) + (neg * (-1));
+	
+	        Console.WriteLine("No of right ans:" +pos);
+		
+		Console.WriteLine("No of wrong ans:" +neg);
+		
+		Console.WriteLine("Marks obtained:" +res);	
+		
+	}
+}
+
+
+
+
+// string array
 using System;
 using System.Collections.Generic;
 using System.Linq;
